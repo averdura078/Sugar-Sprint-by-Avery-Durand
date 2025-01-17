78fb3f237b28d63dabb7bbaf358b39170fd0bfe6
averdura078 <averdura078@SDS-L-1BQLG63>
2025-01-17 11:27:04 -0500

    Add project files.

---
+++ Sugar Sprint by Avery Durand/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace Sugar_Sprint_by_Avery_Durand
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.startButton = new System.Windows.Forms.Button();
+            this.nameInput = new System.Windows.Forms.TextBox();
+            this.enterNameLabel = new System.Windows.Forms.Label();
+            this.instructionsButton = new System.Windows.Forms.Button();
+            this.chefNameLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Interval = 20;
+            this.gameTimer.Tick += new System.EventHandler(this.gameTimer_Tick);
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.titleLabel.Font = new System.Drawing.Font("Jokerman", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLabel.ForeColor = System.Drawing.Color.Black;
+            this.titleLabel.Location = new System.Drawing.Point(-1, -1);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(700, 505);
+            this.titleLabel.TabIndex = 0;
+            this.titleLabel.Text = "Sugar Sprint";
+            this.titleLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // startButton
+            // 
+            this.startButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.startButton.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.startButton.FlatAppearance.BorderSize = 2;
+            this.startButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.startButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.startButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.startButton.Font = new System.Drawing.Font("Jokerman", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startButton.Location = new System.Drawing.Point(267, 270);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(177, 71);
+            this.startButton.TabIndex = 1;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = false;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // nameInput
+            // 
+            this.nameInput.Font = new System.Drawing.Font("Jokerman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nameInput.Location = new System.Drawing.Point(267, 190);
+            this.nameInput.Name = "nameInput";
+            this.nameInput.Size = new System.Drawing.Size(177, 42);
+            this.nameInput.TabIndex = 2;
+            this.nameInput.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // enterNameLabel
+            // 
+            this.enterNameLabel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.enterNameLabel.Font = new System.Drawing.Font("Jokerman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.enterNameLabel.Location = new System.Drawing.Point(238, 136);
+            this.enterNameLabel.Name = "enterNameLabel";
+            this.enterNameLabel.Size = new System.Drawing.Size(232, 39);
+            this.enterNameLabel.TabIndex = 3;
+            this.enterNameLabel.Text = "Enter your name:";
+            this.enterNameLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // instructionsButton
+            // 
+            this.instructionsButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.instructionsButton.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.instructionsButton.FlatAppearance.BorderSize = 2;
+            this.instructionsButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.instructionsButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.instructionsButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.instructionsButton.Font = new System.Drawing.Font("Jokerman", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.instructionsButton.Location = new System.Drawing.Point(216, 370);
+            this.instructionsButton.Name = "instructionsButton";
+            this.instructionsButton.Size = new System.Drawing.Size(268, 71);
+            this.instructionsButton.TabIndex = 6;
+            this.instructionsButton.Text = "Instructions";
+            this.instructionsButton.UseVisualStyleBackColor = false;
+            this.instructionsButton.Click += new System.EventHandler(this.instructionsButton_Click);
+            // 
+            // chefNameLabel
+            // 
+            this.chefNameLabel.BackColor = System.Drawing.Color.Transparent;
+            this.chefNameLabel.Font = new System.Drawing.Font("Jokerman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chefNameLabel.Location = new System.Drawing.Point(238, -1);
+            this.chefNameLabel.Name = "chefNameLabel";
+            this.chefNameLabel.Size = new System.Drawing.Size(232, 39);
+            this.chefNameLabel.TabIndex = 7;
+            this.chefNameLabel.Text = "Chef";
+            this.chefNameLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.chefNameLabel.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.ClientSize = new System.Drawing.Size(700, 500);
+            this.Controls.Add(this.chefNameLabel);
+            this.Controls.Add(this.instructionsButton);
+            this.Controls.Add(this.enterNameLabel);
+            this.Controls.Add(this.nameInput);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.titleLabel);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Sugar Sprint";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer gameTimer;
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.TextBox nameInput;
+        private System.Windows.Forms.Label enterNameLabel;
+        private System.Windows.Forms.Button instructionsButton;
+        private System.Windows.Forms.Label chefNameLabel;
+    }
+}
+
---
+++ Sugar Sprint by Avery Durand/Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//Sugar Sprint game by Avery Durand
+//FINAL PROJECT ICS3U
+//Mr. T
+
+namespace Sugar_Sprint_by_Avery_Durand
+{
+    public partial class Form1 : Form
+    {
+        //ground
+        Rectangle ground = new Rectangle(0, 350, 550, 100);
+        SolidBrush greenBrush = new SolidBrush(Color.SpringGreen);
+
+        //player
+        Rectangle chef = new Rectangle(10, 335, 20, 20);
+        SolidBrush yellowBrush = new SolidBrush(Color.Yellow);
+        int chefSpeed = 10;
+        int jumpUpOrDown= 0;
+
+        //obstacles
+
+        //falling obstacles
+
+        //candies
+
+        //keys
+        bool leftPressed = false;
+        bool rightPressed = false;
+        bool jumpPressed = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Left:
+                    leftPressed = false;
+                    break;
+                case Keys.Right:
+                    rightPressed = false;
+                    break;
+                case Keys.Up:
+                    jumpPressed = false;
+                    break;
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Left:
+                    leftPressed = true;
+                    break;
+                case Keys.Right:
+                    rightPressed = true;
+                    break;
+                case Keys.Space:
+                    if (jumpPressed == false)
+                    {
+                        jumpPressed = true;
+                    }
+                    break;
+            }
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.FillRectangle(greenBrush, ground);
+            e.Graphics.FillRectangle(yellowBrush, chef);
+        }
+
+        private void gameTimer_Tick(object sender, EventArgs e)
+        {
+            //move player
+            if (rightPressed == true && chef.X < this.Width - chef.Width)
+            {
+                chef.X += chefSpeed;
+            }
+            if (leftPressed == true && chef.X > 0)
+            {
+                chef.X -= chefSpeed;
+            }
+            if (jumpPressed == true)
+            {
+                jumpUpOrDown++;
+                if (jumpUpOrDown < 5)
+                {
+                    //up
+                    chef.Y -= chefSpeed;
+                }
+                if (jumpUpOrDown > 5)
+                {
+                    //down
+                    chef.Y += chefSpeed;
+                }
+                
+            }
+
+            Refresh();
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            //start game
+            gameTimer.Start();
+
+            //hide menu
+            titleLabel.Visible = false;
+            enterNameLabel.Visible = false;
+            nameInput.Visible = false;
+            startButton.Visible = false;
+            instructionsButton.Visible = false;
+
+            //show name
+            chefNameLabel.Text += " " + Convert.ToString(nameInput.Text);
+            chefNameLabel.Visible = true;
+
+            this.Focus();
+        }
+
+        private void instructionsButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
